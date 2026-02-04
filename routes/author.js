02055a191a6653b3f8c2f51c60f56cc3267612a8
TheBighi <thebighix@gmail.com>
2026-02-04 20:19:03 +0200

    fix

--- routes/author.js
+++ routes/author.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const authorControllerClass = require('../controllers/author')
 const authorController = new(authorControllerClass)
 
-router.get('/author/:author_id', authorController.getAuthorById(req, res))
+router.get('/author/:author_id', (req, res) => authorController.getAuthorById(req, res))
 
 module.exports = router
